1d50f3dde8558818e6198ee586530072045ec80f
JuanTriana27 <trianajuan28@gmail.com>
2025-06-16 12:30:53 -0500

    prueba1

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,10 @@
 // src/api/axiosConfig.js
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL,
     headers: {
         "Content-Type": "application/json",
     },
